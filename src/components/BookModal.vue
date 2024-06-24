ba38d4effe40860f206fa77771405a07c533a6a1
Jashqtieee <instajoshua04@gmail.com>
2024-06-24 15:50:41 +0800

    added the book covers for the eBook Page

---
+++ src/components/BookModal.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="modal fade" id="bookModal" tabindex="-1" aria-labelledby="bookModalLabel" aria-hidden="true"
+    ref="bookModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="bookModalLabel">{{ book.title }}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <img :src="book.image" :alt="book.title" class="img-fluid mb-3" />
+          <p><strong>Author:</strong> {{ book.author }}</p>
+          <p>{{ book.description }}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Read Now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookModal',
+  props: {
+    book: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    showModal() {
+      this.$nextTick(() => {
+        const modal = new window.bootstrap.Modal(this.$refs.bookModal);
+        modal.show();
+      });
+    },
+    hideModal() {
+      const modal = new window.bootstrap.Modal(this.$refs.bookModal);
+      modal.hide();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.modal-content {
+  background-color: #1c1c1c;
+  color: #f1c503;
+}
+
+.modal-footer .btn-primary {
+  background-color: #f1c503;
+  border: none;
+  color: #110f0d;
+}
+</style>
--- src/views/eBooksPage.vue
+++ src/views/eBooksPage.vue
@@ -1,101 +1,97 @@
 <template>
   <HeaderComponent />
-  <div class="ebooks-page">
-    <!-- Featured Book Section -->
-    <section class="featured-book text-center text-white py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <img src="../assets/images/logo_icon_design_1.gif" alt="Featured Book" class="img-fluid rounded" />
-          </div>
-          <div class="col-md-6 d-flex flex-column justify-content-center">
-            <h1 class="display-4">Featured Book Title</h1>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores soluta excepturi vero,
-              harum labore quos libero iure natus error omnis iusto provident laudantium culpa quis eum mollitia
-              assumenda ipsa veritatis consectetur ea. Maiores quia nam distinctio iure aspernatur, illo excepturi
-              eligendi nesciunt voluptatibus consequatur aliquid facilis, animi quo officiis ex.</p>
-            <div class="buttons d-flex mt-2">
-              <button class="btn btn-primary m-2"> <i class="bi bi-play-fill" style="color: black; padding: 0%; "></i>
-                Play
-                Now </button>
-              <button class="btn btn-secondary m-2"><i class="bi bi-info-circle" style=" padding: 0%;"></i> More
-                Info</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
 
-    <!-- Books Grid Section -->
-    <section class="books-grid py-5 bg-light">
-      <div class="container">
-        <h2 class="section-title text-dark mb-4">Popular eBooks</h2>
-        <div class="row">
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-4" v-for="book in books" :key="book.id">
-            <div class="card h-100">
-              <img :src="book.image" :alt="book.title" class="card-img-top" />
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">{{ book.title }}</h5>
-                <p class="card-text">{{ book.author }}</p>
-                <button class="btn btn-primary mt-auto" @click="openModal(book)">Read Now</button>
+  <body>
+    <div class="ebooks-page">
+      <!-- Featured Book Section -->
+      <section class="featured-book text-center text-white py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <img src="../assets/images/ebooks-cover/5.png" alt="Featured Book" class="img-fluid rounded" />
+            </div>
+            <div class="col-md-6 d-flex flex-column justify-content-center">
+              <h1 class="display-4">A Field Book of the Stars</h1>
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores soluta excepturi vero,
+                harum labore quos libero iure natus error omnis iusto provident laudantium culpa quis eum mollitia
+                assumenda ipsa veritatis consectetur ea. Maiores quia nam distinctio iure aspernatur, illo excepturi
+                eligendi nesciunt voluptatibus consequatur aliquid facilis, animi quo officiis ex.</p>
+              <div class="buttons d-flex mt-2">
+                <button class="btn btn-primary m-2"> <i class="bi bi-play-fill" style="color: black; padding: 0%; "></i>
+                  Read
+                  Now </button>
+                <button class="btn btn-secondary m-2"><i class="bi bi-info-circle" style=" padding: 0%;"></i> More
+                  Info</button>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <!-- Book Details Modal -->
-    <div class="modal fade" id="bookModal" tabindex="-1" aria-labelledby="bookModalLabel" aria-hidden="true"
-      ref="bookModal">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="bookModalLabel">{{ selectedBook.title }}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <img :src="selectedBook.image" :alt="selectedBook.title" class="img-fluid mb-3" />
-            <p><strong>Author:</strong> {{ selectedBook.author }}</p>
-            <p>{{ selectedBook.description }}</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Read Now</button>
+      <!-- Books Grid Section -->
+      <section class="books-grid py-5">
+        <div class="container">
+          <h2 class="section-title mb-4">Popular eBooks</h2>
+          <div class="row">
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4" v-for="book in books" :key="book.id">
+              <div class="card h-100">
+                <img :src="book.image" :alt="book.title" class="card-img-top" />
+                <div class="card-body d-flex flex-column">
+                  <h5 class="card-title">{{ book.title }}</h5>
+                  <p class="card-text">{{ book.author }}</p>
+                  <button class="btn btn-primary mt-auto" @click="openModal(book)">Read Now</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+
+      <!-- Book Details Modal -->
+      <BookModal :book="selectedBook" ref="bookModal" />
     </div>
-  </div>
+  </body>
 </template>
 
 <script>
 import HeaderComponent from './HeaderComponent.vue';
+import BookModal from '../components/BookModal.vue';
 
 export default {
   name: "eBooksPage",
   components: {
-    HeaderComponent
+    HeaderComponent,
+    BookModal,
   },
   data() {
     return {
       books: [
-        { id: 1, title: "Book 1", author: "Author 1", image: "../assets/images/bible.png", description: "Description for Book 1" },
-        { id: 2, title: "Book 2", author: "Author 2", image: "../assets/images/bible.png", description: "Description for Book 2" },
-        { id: 3, title: "Book 3", author: "Author 3", image: "../assets/images/bible.png", description: "Description for Book 3" },
-        { id: 4, title: "Book 4", author: "Author 4", image: "../assets/images/bible.png", description: "Description for Book 4" },
-        { id: 5, title: "Book 5", author: "Author 5", image: "../assets/images/bible.png", description: "Description for Book 5" },
-        { id: 6, title: "Book 6", author: "Author 6", image: "../assets/images/bible.png", description: "Description for Book 6" },
-        { id: 7, title: "Book 7", author: "Author 7", image: "../assets/images/bible.png", description: "Description for Book 7" },
-        { id: 8, title: "Book 8", author: "Author 8", image: "../assets/images/bible.png", description: "Description for Book 8" }
+        { id: 1, title: "A Brief Account of the Rise and Progress of the People Called Quakers", author: "William Pen", image: require('../assets/images/ebooks-cover/1.png'), description: "Description for Book 1" },
+        { id: 2, title: "A Comparative View of Religions", author: "Johannes Henricus Scholten", image: require('../assets/images/ebooks-cover/2.png'), description: "Description for Book 2" },
+        { id: 3, title: "A Critical Exposition of the Popular Jihad", author: "Moulavi Gheragh Ali", image: require('../assets/images/ebooks-cover/3.png'), description: "Description for Book 3" },
+        { id: 4, title: "A Dictionary of Symbols", author: "J.E. Cirlot", image: require('../assets/images/ebooks-cover/4.png'), description: "Description for Book 4" },
+        { id: 5, title: "A Field Book of the Stars", author: "William Tyler Olcott", image: require('../assets/images/ebooks-cover/5.png'), description: "Description for Book 5" },
+        { id: 6, title: "A Genesis Apocryphon", author: "Nahman Avigad and Yigael Yadin", image: require('../assets/images/ebooks-cover/6.png'), description: "Description for Book 6" },
+        { id: 7, title: "A History of American Christianity", author: "Leonard Woolsey Bacon", image: require('../assets/images/ebooks-cover/7.png'), description: "Description for Book 7" },
+        { id: 8, title: "A History of Indian Philosophy, Volume 1", author: "Surendranath Dasgupta", image: require('../assets/images/ebooks-cover/8.png'), description: "Description for Book 8" }
       ],
-      selectedBook: {}
+      selectedBook: {} // Initialize an empty object for the selected book
     };
   },
+  methods: {
+    openModal(book) {
+      this.selectedBook = book; // Assign the clicked book to the selectedBook data property
+      this.$refs.bookModal.showModal(); // Call the showModal method in the BookModal component
+    }
+  }
 };
 </script>
 
 <style scoped>
+body {
+  background-color: #110f0d;
+}
+
 .ebooks-page {
   margin-top: 131px;
   font-family: "Glegoo", sans-serif;
@@ -115,6 +111,16 @@ export default {
   color: #f1c503;
 }
 
+.section-title h2 {
+  font-size: 14px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 20px;
+  padding-bottom: 20px;
+  position: relative;
+  display: inline-block;
+}
+
 .card {
   border: none;
   overflow: hidden;
@@ -124,6 +130,12 @@ export default {
 
 .card-img-top {
   height: 300px;
+  border-bottom: 2px solid #f1c503;
+}
+
+.card-img-top img {
+  width: 80%;
+  height: 100%;
   object-fit: cover;
 }
 
@@ -142,9 +154,5 @@ export default {
   color: #110f0d;
   font-family: "Nunito", sans-serif;
   font-weight: 700;
-}
-
-.lead {
-  font-size: 14px;
 }
 </style>
