28ffcf654995ba1f0798b3f565739e6a0db14679
Chaaancreamm <cce7990@students.uc-bcf.edu.ph>
2024-06-25 15:31:38 +0800

    Fixed designs in some parts of the book carousel

--- src/components/eBooks/BooksCarousel.vue
+++ src/components/eBooks/BooksCarousel.vue
@@ -12,7 +12,7 @@
                   <div class="card-body d-flex flex-column">
                     <h5 class="card-title"><b>{{ book.title }}</b></h5>
                     <p class="card-text">{{ book.author }}</p>
-                    <button class="btn btn-primary mt-auto" @click="openModal(book)">Read Now</button>
+                    <button class="btn btn-primary" @click="openModal(book)">Read Now</button>
                     <div class="rating">
                       <font-awesome-icon v-for="n in 5" :key="n" :icon="[book.userRating >= n ? 'fas' : 'far', 'star']"
                         class="fa-star" @click="setRating(book, n)" />
@@ -89,7 +89,7 @@ export default {
 .card {
   display: flex;
   flex-direction: column;
-  height: 650px;
+  height: 600px;
   width: 270px;
   border: none;
   overflow: hidden;
@@ -113,7 +113,7 @@ export default {
 .rating {
   display: flex;
   gap: 2px;
-  margin-bottom: 10px;
+  margin-bottom: 0;
   cursor: pointer;
 }
 
@@ -136,7 +136,7 @@ textarea {
   color: #110f0d;
   font-family: "Nunito", sans-serif;
   font-weight: 700;
-  margin-bottom: 80px;
+  margin-bottom: 20px;
 }
 
 .carousel-control-prev,
