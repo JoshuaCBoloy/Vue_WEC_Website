2f2b842e4b21656e38cfb34ae2e48cb9b14518ff
Chaaancreamm <cce7990@students.uc-bcf.edu.ph>
2024-06-26 21:10:55 +0800

    refined the deseign of the footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,46 +5,46 @@
         <div class="col-md-2 mb-4 d-flex flex-column align-items-center">
           <h5>Wisdom e<span>B</span>ooks Club</h5>
           <ul class="list-unstyled">
-            <li><a href="mailto:support@wisdomebooksclub.com" class="text-white">Email: support@wisdomebooksclub.com</a></li>
+            <li><a href="mailto:support@wisdomebooksclub.com">Email: support@wisdomebooksclub.com</a></li>
           </ul>
           <div class="social-icons d-flex justify-content-center">
-            <a href="#" class="text-white mx-2"><i class="fab fa-facebook-f"></i></a>
-            <a href="#" class="text-white mx-2"><i class="fab fa-twitter"></i></a>
-            <a href="#" class="text-white mx-2"><i class="fab fa-instagram"></i></a>
-            <a href="#" class="text-white mx-2"><i class="fab fa-youtube"></i></a>
+            <a href="#" class="mx-2"><i class="fab fa-twitter"></i></a>
+            <a href="#" class="mx-2"><i class="fab fa-instagram"></i></a>
+            <a href="#" class="mx-2"><i class="fab fa-youtube"></i></a>
+            <a href="#" class="mx-2"><i class="fab fa-facebook-f"></i></a>
           </div>
         </div>
         <div class="col-md-2 mb-4 d-flex flex-column align-items-center">
           <h5>Useful Links</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> About Us</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Contact Us</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Terms of Service</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Privacy Policy</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>About Us</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Contact Us</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Terms of Service</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Privacy Policy</a></li>
           </ul>
         </div>
         <div class="col-md-2 mb-4 d-flex flex-column align-items-center">
           <h5>eBooks</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Book Library</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Book Categories</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Book Library</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Book Categories</a></li>
           </ul>
         </div>
         <div class="col-md-2 mb-4 d-flex flex-column align-items-center">
           <h5>eStudy Bible</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Parallel: Side-by-Side Bible</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> King James Hebrew-Greek Interlinear Bible</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Cross-Reference Bible</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Wisdom Bible Plus</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Bible Atlas</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Parallel: Side-by-Side Bible</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>King James Hebrew-Greek Interlinear Bible</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Cross-Reference Bible</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Wisdom Bible Plus</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Bible Atlas</a></li>
           </ul>
         </div>
         <div class="col-md-2 mb-4 d-flex flex-column align-items-center">
           <h5>Pearls of Wisdom</h5>
           <ul class="list-unstyled">
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Ancient Hebrew Calendar</a></li>
-            <li><a href="#" class="text-white"><i class="fas fa-arrow-right"></i> Wisdom Blogs</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Ancient Hebrew Calendar</a></li>
+            <li><a href="#"><i class="fas fa-arrow-right me-2"></i>Wisdom Blogs</a></li>
           </ul>
         </div>
       </div>
@@ -90,14 +90,20 @@ a {
   font-size: 13px;
   text-decoration: none;
   position: relative;
-  padding-left: 15px;
+  padding-left: 0; /* Removed left padding to align the arrow with text */
+  transition: background-color 0.3s; /* Smooth transition for hover effect */
+  color: #fff
+}
+
+a:hover {
+  width: 108%;
+  background-color: #fff; /* Background effect on hover */
+  border-radius: 15px; /* Optional: Rounded corners */
+  color: #f1c503; /* Optional: Change text color on hover */
 }
 
 a i.fa-arrow-right {
-  position: absolute;
-  left: 0;
-  top: 50%;
-  transform: translateY(-50%);
+  margin-right: 5px; /* Added right margin to create space between arrow and text */
   color: #f1c503;
   font-size: 0.8rem;
 }
