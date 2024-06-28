2f9813b3125713a9f4e42acaa264e3022743daa1
C-ESTIBAR5 <cce7990@students.uc-bcf.edu.ph>
2024-06-28 13:25:03 +0800

    fixed and refined some designs for the parallel page

--- src/views/ParallelPage.vue
+++ src/views/ParallelPage.vue
@@ -5,35 +5,55 @@
       <div class="parallel-header text-center my-4">
         <h1>Parallel: Side-by-Side Bible</h1>
       </div>
-      <nav class="navbar navbar-expand-lg justify-content-center">
-        <ul class="nav justify-content-center">
-          <li class="dropdown">
-            <div class="dropdown-toggle">
-              <h1 id="selected-book">Genesis</h1>
-            </div>
-            <ul class="dropdown-menu">
-              <li v-for="book in books" :key="book">{{ book }}</li>
-            </ul>
-          </li>
-          <li class="dropdown">
-            <div class="dropdown-toggle">
-              <h1 id="selected-chapter">1</h1>
-            </div>
-            <ul class="dropdown-menu">
-              <li v-for="chapter in chapters" :key="chapter">{{ chapter }}</li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
 
       <div class="row justify-content-center">
         <div class="col-md-6 col-lg-5">
+          <nav class="navbar navbar-expand-lg justify-content-center">
+            <ul class="nav justify-content-center">
+              <li class="dropdown">
+                <div class="dropdown-toggle">
+                  <h1 id="selected-book">Genesis</h1>
+                </div>
+                <ul class="dropdown-menu">
+                  <li v-for="book in books" :key="book">{{ book }}</li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <div class="dropdown-toggle">
+                  <h1 id="selected-chapter">1</h1>
+                </div>
+                <ul class="dropdown-menu">
+                  <li v-for="chapter in chapters" :key="chapter">{{ chapter }}</li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
           <div class="content-section">
             <h2>Left Column</h2>
             <p>Content for the left column goes here.</p>
           </div>
         </div>
         <div class="col-md-6 col-lg-5">
+          <nav class="navbar navbar-expand-lg justify-content-center">
+            <ul class="nav justify-content-center">
+              <li class="dropdown">
+                <div class="dropdown-toggle">
+                  <h1 id="selected-book">Genesis</h1>
+                </div>
+                <ul class="dropdown-menu">
+                  <li v-for="book in books" :key="book">{{ book }}</li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <div class="dropdown-toggle">
+                  <h1 id="selected-chapter">1</h1>
+                </div>
+                <ul class="dropdown-menu">
+                  <li v-for="chapter in chapters" :key="chapter">{{ chapter }}</li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
           <div class="content-section">
             <h2>Right Column</h2>
             <p>Content for the right column goes here.</p>
@@ -46,34 +66,95 @@
 </template>
 
 <script>
-import NavbarComponent from '@/components/NavbarComponent.vue';
-import FooterComponent from '@/components/FooterComponent.vue';
+import NavbarComponent from "@/components/NavbarComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
   components: {
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
   },
   data() {
     return {
       books: [
-        'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', 'Ruth',
-        '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra',
-        'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Solomon',
-        'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos',
-        'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah',
-        'Malachi', 'Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1 Corinthians', '2 Corinthians',
-        'Galatians', 'Ephesians', 'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians',
-        '1 Timothy', '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter', '2 Peter',
-        '1 John', '2 John', '3 John', 'Jude', 'Revelation'
+        "Genesis",
+        "Exodus",
+        "Leviticus",
+        "Numbers",
+        "Deuteronomy",
+        "Joshua",
+        "Judges",
+        "Ruth",
+        "1 Samuel",
+        "2 Samuel",
+        "1 Kings",
+        "2 Kings",
+        "1 Chronicles",
+        "2 Chronicles",
+        "Ezra",
+        "Nehemiah",
+        "Esther",
+        "Job",
+        "Psalms",
+        "Proverbs",
+        "Ecclesiastes",
+        "Song of Solomon",
+        "Isaiah",
+        "Jeremiah",
+        "Lamentations",
+        "Ezekiel",
+        "Daniel",
+        "Hosea",
+        "Joel",
+        "Amos",
+        "Obadiah",
+        "Jonah",
+        "Micah",
+        "Nahum",
+        "Habakkuk",
+        "Zephaniah",
+        "Haggai",
+        "Zechariah",
+        "Malachi",
+        "Matthew",
+        "Mark",
+        "Luke",
+        "John",
+        "Acts",
+        "Romans",
+        "1 Corinthians",
+        "2 Corinthians",
+        "Galatians",
+        "Ephesians",
+        "Philippians",
+        "Colossians",
+        "1 Thessalonians",
+        "2 Thessalonians",
+        "1 Timothy",
+        "2 Timothy",
+        "Titus",
+        "Philemon",
+        "Hebrews",
+        "James",
+        "1 Peter",
+        "2 Peter",
+        "1 John",
+        "2 John",
+        "3 John",
+        "Jude",
+        "Revelation",
       ],
       chapters: Array.from({ length: 50 }, (_, i) => i + 1), // Assuming Genesis has 50 chapters
     };
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
+h1 {
+  font-size: 24px;
+  color: #f1c503;
+}
 .custom-container {
   margin: 20px auto;
   padding: 20px;
@@ -92,7 +173,7 @@ export default {
 }
 
 .parallel-header h1 {
-  font-size: 27px;
+  font-size: 24px;
   font-weight: 400;
   color: #000;
 }
